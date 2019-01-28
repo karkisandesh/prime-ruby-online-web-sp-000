5eb906368cc0ede4e2fc7dfa8d222fac267903fc
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-28 20:44:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,19 @@
-def prime?(n)
-  if n <= 1
-    return false
-  elsif n <= 3
-    return true
-    else (2..n/2).none? do |x|
-      n % x == 0
-      end
-  end
+# def prime?(n)
+#   if n <= 1
+#     return false
+#   elsif n <= 3
+#     return true
+#     else (2..n/2).none? do |x|
+#       n % x == 0
+#       end
+#   end
+# end
+
+def prime(n)
+  if n <= 1 
+    return false 
+    if n == 2 || n == 3 
+      return true 
+    else n % 2 == 0 || n % 3 == 0 
+      return true
 end
